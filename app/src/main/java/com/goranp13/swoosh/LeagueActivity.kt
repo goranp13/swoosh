61ea87f27970e177a35eaa28f6e06bf64e223d99
goranp13 <goranp13@gmail.com>
2018-12-02 16:25:15 +0100

    lesson 7

--- app/src/main/java/com/goranp13/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/goranp13/swoosh/LeagueActivity.kt
@@ -4,16 +4,51 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeadue = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun leagueNextClicked(view: View){
-        val skillActivity = Intent(this, SkillActivity::class.java)
-        startActivity(skillActivity)
+    fun onMensClicked(view: View) {
+        btnWomansLeague.isChecked = false
+        btnCoedLeague.isChecked = false
+
+        selectedLeadue = "mens"
+    }
+
+    fun onWomansClicked(view: View) {
+        btnMensLeague.isChecked = false
+        btnCoedLeague.isChecked = false
+
+        selectedLeadue = "Womens"
+
+    }
+
+    fun onCoedsClicked(view: View) {
+        btnMensLeague.isChecked = false
+        btnWomansLeague.isChecked = false
+
+        selectedLeadue = "co-ed"
+
+    }
+
+
+    fun leagueNextClicked(view: View) {
+
+        if (selectedLeadue != ""){
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            skillActivity.putExtra(EXTRA_LEAGUE, selectedLeadue)
+            startActivity(skillActivity)
+        }else {
+            Toast.makeText(this, "Please select your league!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
